3a7011871c2d6b1e7e542f9349d3f0b8f42381ee
alainjurado91 <e.alain.jurado@gmail.com>
2024-08-11 22:07:36 -0600

    primer commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const textArea = document.querySelector(".container-inputSection-textArea");
+const message = document.querySelector(".container-outputSection-outputPresentation");
+
+/*
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
+*/
+
+function encryptButton(){
+    const encryptedText = encrypt(textArea.value);
+    message.value = encryptedText;
+    textArea.value = "";
+    message.style.backgroundImage = "none";
+}
+
+function encrypt(encryptedString){
+    let matrizCodigo = [["e","enter"], ["i","imes"], ["a","ai"], ["o","ober"], ["u","ufat"]];
+    encryptedString = encryptedString.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(encryptedString.includes(matrizCodigo[i][0])){
+            encryptedString = encryptedString.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+
+    return encryptedString; 
+}
+
+function decryptButton(){
+    const encryptedText = decrypt(textArea.value);
+    message.value = encryptedText;
+    textArea.value = "";
+}
+
+
+function decrypt(decryptedString){
+    let matrizCodigo = [["e","enter"], ["i","imes"], ["a","ai"], ["o","ober"], ["u","ufat"]];
+    decryptedString = decryptedString.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(decryptedString.includes(matrizCodigo[i][1])){
+            decryptedString = decryptedString.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+
+    return decryptedString; 
+}
+
+
